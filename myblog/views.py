2169ccf8a3b4698cee946b24b30b5e412685e3a0
oriechinedu <oriechinedu@gmail.com>
2018-03-20 17:27:26 +0100

    add post edit

--- myblog/views.py
+++ myblog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -20,7 +21,36 @@ def my_profile(request):
 
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
+
     if post:
-        return render(request, 'myblog/post_detail.html', {'post': post})
+        return render(request, 'myblog/post_detail.html', {'post': post, })
     else:
         return render(request, 'myblog/post_detail.html', {'pk': pk})
+
+
+def post_new(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+        return render(request, 'myblog/new_post.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = Post.objects.get(pk=pk)
+    form = PostForm(request.POST, instance=post)
+    if form.is_valid():
+        post = form.save(commit=False)
+        post.author = request.user
+        post.published_date = timezone.now()
+        post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'myblog/new_post.html', {'form': form})
